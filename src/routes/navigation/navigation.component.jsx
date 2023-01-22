151dada4bc939751848e1454b0189a56ca123213
Jad Taha <jadtaha.de@gmail.com>
2023-01-22 11:15:17 +0100

    CartDropdown & Styles  --version 01.01.40

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,6 +5,7 @@ import { ReactComponent as OneMJLogo } from '../../assets/1Million JOURNEY.svg';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropDown from '../../components/cart-dropdown/cart-dropdown.component';
 
 const Navigation = () => {
   const { currentUser, setCurrentUser } = useContext(UserContext);
@@ -33,6 +34,7 @@ const Navigation = () => {
           )}
           <CartIcon></CartIcon>
         </div>
+        <CartDropDown></CartDropDown>
       </div>
       <Outlet></Outlet>
     </Fragment>

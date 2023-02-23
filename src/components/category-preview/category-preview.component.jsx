7e2923fd753b86e43999da20685d6128fe7f177b
jadTahaGit <jadtaha.de@gmail.com>
2023-02-23 09:49:06 +0100

    Links - Style Fixes --version 01.01.97

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,12 +1,16 @@
 import './category-preview.styles.scss';
 
 import ProductCard from '../product-card/product-card.component';
+import { Link } from 'react-router-dom';
 
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title"> {title}</span>
+        <Link className="title" to={title}>
+          {' '}
+          {title}
+        </Link>
       </h2>
       <div className="preview">
         {products
